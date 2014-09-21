cfe6c7dee94f443ee93d9139ac0b1d2a00e59f11
David Sveningsson <ext@sidvind.com>
2014-09-21 15:46:22 +0200

    fills controller if 'to' is only action

--- proutes.php
+++ proutes.php
@@ -71,28 +71,36 @@ class ProuterLeafContext {
 	}
 
 	public function get($pattern, array $options=[]){
-		$options = array_merge($this->options, ['to' => $this->options['to'] . '#' . prouter_actionname($pattern)], $options);
-		$this->router->method("{$this->namespace}/{$pattern}", 'GET', $options);
+		$this->method($pattern, 'GET', $options);
 	}
 
 	public function post($pattern, array $options=[]){
-		$options = array_merge($this->options, ['to' => $this->options['to'] . '#' . prouter_actionname($pattern)], $options);
-		$this->router->method("{$this->namespace}/{$pattern}", 'POST', $options);
+		$this->method($pattern, 'POST', $options);
 	}
 
 	public function put($pattern, array $options=[]){
-		$options = array_merge($this->options, ['to' => $this->options['to'] . '#' . prouter_actionname($pattern)], $options);
-		$this->router->method("{$this->namespace}/{$pattern}", 'PUT', $options);
+		$this->method($pattern, 'PUT', $options);
 	}
 
 	public function delete($pattern, array $options=[]){
-		$options = array_merge($this->options, ['to' => $this->options['to'] . '#' . prouter_actionname($pattern)], $options);
-		$this->router->method("{$this->namespace}/{$pattern}", 'DELETE', $options);
+		$this->method($pattern, 'DELETE', $options);
 	}
 
 	public function method($pattern, $method, array $options=[]){
-		$options = array_merge($this->options, ['to' => $this->options['to'] . '#' . prouter_actionname($pattern)], $options);
+		$default_to = $this->options['to'] . '#' . prouter_actionname($pattern);
+		$options = array_merge($this->options, ['to' => $default_to], $options);
+		$options = $this->fill_to($options);
 		$this->router->method("{$this->namespace}/{$pattern}", $method, $options);
+	}
+
+	/**
+	 * If 'to' is '#foo' it fills the controller from context.
+	 */
+	protected function fill_to($options){
+		if ( $options['to'][0] == '#' ){
+			$options['to'] = $this->options['to'] . $options['to'];
+		}
+		return $options;
 	}
 };
 
