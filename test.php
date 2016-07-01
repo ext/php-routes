268660ac7720780a00026bde9ad3f0b61d06beb1
David Sveningsson <ext@sidvind.com>
2016-07-01 23:50:40 +0200

    tests: migrate simple dispatch tests to phpunit

--- test.php
+++ test.php
@@ -1,37 +1,13 @@
 <?php
 
 require_once('proutes.php');
-
-class TestProuter extends Prouter {
-	function test_match($pattern, $method, $expected_controller, $expected_action, array $expected_args=[]){
-		echo "  - Testing $method '$pattern' .. ";
-		list($actual_controller, $actual_action, $args) = $this->match($pattern, $method);
-
-		if ( $actual_controller != $expected_controller ){
-			echo "failed (controller)\n      Expected:   $expected_controller\n      Actual:     $actual_controller\n";
-			return;
-		}
-
-		if ( $actual_action != $expected_action ){
-			echo "failed (action)\n      Expected:   $expected_action\n      Actual:     $actual_action\n";
-			return;
-		}
-
-		/** @todo test arguments */
-
-		echo "ok\n";
-	}
-};
-
 $router = new TestProuter();
 
 echo "Testing dispatching\n";
 $router->clear();
-$router->method('foo', 'GET', ['to' => 'Index']);
 $router->use_namespace('namespace', [], function($x){
 	$x->method('bar', 'GET', ['to' => 'Test']);
 });
-$router->test_match('/foo', 'GET', 'Index', 'foo');
 $router->test_match('/namespace/bar', 'GET', 'Test', 'namespace_bar');
 
 $router->clear();
---
+++ tests/RouterDispatchTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace RouterDispatch;
+
+class RouterDispatchFunctionTest extends \PHPUnit_Framework_TestCase {
+	public $router;
+
+	public function setUp(){
+		$this->router = new \Sidvind\PHPRoutes\Router();
+	}
+
+	protected function path_function($pattern){
+		return $this->router->method($pattern, 'GET', ['as' => 'test_pattern']);
+	}
+
+	public function test_simple(){
+		$this->router->method('foo', 'GET', ['to' => 'Test#action']);
+		$match = $this->router->match('/foo', 'GET');
+		$this->assertTrue((boolean)$match);
+		$this->assertEquals('Test', $match->controller);
+		$this->assertEquals('action', $match->action);
+		$this->assertEquals([], $match->args);
+		$this->assertEquals(false, $match->format);
+	}
+
+	public function test_method(){
+		$this->router->method('foo', 'PATCH', ['to' => 'Test#action']);
+		$match = $this->router->match('/foo', 'PATCH');
+		$this->assertTrue((boolean)$match);
+		$this->assertEquals('Test', $match->controller);
+		$this->assertEquals('action', $match->action);
+		$this->assertEquals([], $match->args);
+		$this->assertEquals(false, $match->format);
+	}
+
+	public function test_wrong_method(){
+		$this->router->method('foo', 'PATCH', ['to' => 'Test#action']);
+		$match = $this->router->match('/foo', 'GET');
+		$this->assertFalse((boolean)$match);
+	}
+
+	public function test_args(){
+		$this->router->method('foo/:id', 'GET', ['to' => 'Test#action']);
+		$match = $this->router->match('/foo/7', 'GET');
+		$this->assertTrue((boolean)$match);
+		$this->assertEquals('Test', $match->controller);
+		$this->assertEquals('action', $match->action);
+		$this->assertEquals(['id' => 7], $match->args);
+		$this->assertEquals(false, $match->format);
+	}
+
+	public function test_format(){
+		$this->router->method('foo', 'GET', ['to' => 'Test#action']);
+		$match = $this->router->match('/foo.json', 'GET');
+		$this->assertTrue((boolean)$match);
+		$this->assertEquals('Test', $match->controller);
+		$this->assertEquals('action', $match->action);
+		$this->assertEquals([], $match->args);
+		$this->assertEquals('application/json', $match->format);
+	}
+
+	protected function assertMatch($url, $method, $controller, $action, array $args=[]){
+		$match = $this->router->match($url, $method);
+		$this->assertTrue((boolean)$match, "URL {$url} should match a route");
+		$this->assertEquals($controller, $match->controller);
+		$this->assertEquals($action, $match->action);
+		$this->assertEquals($args, $match->args);
+	}
+}
