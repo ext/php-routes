22353d7022e1d2f6d53a051c87926b286ad4e30c
David Sveningsson <ext@sidvind.com>
2016-10-23 19:26:11 +0200

    example: split files

---
+++ example/controllers/index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ExampleApplication;
+
+class IndexController {
+	public function index(){
+		echo "index\n";
+	}
+
+	public function about(){
+		echo "about\n";
+	}
+}
---
+++ example/controllers/post.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ExampleApplication;
+
+class PostController {
+	public function show($id){
+		echo "show post $id\n";
+	}
+}
